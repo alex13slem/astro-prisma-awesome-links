4a10ac0b7778ffaeab60b9d0bc58fc68a2ad6642
alex13slem <a.s.scherba@yandex.by>
2024-02-10 22:15:05 +0300

    asd

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -3,10 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 const apolloClient = new ApolloClient({
   uri: import.meta.env.URL + '/api/graphql',
   cache: new InMemoryCache(),
-  onError: ({ networkError, graphQLErrors }) => {
-    console.log('graphQLErrors', graphQLErrors);
-    console.log('networkError', networkError);
-  },
+  ssrMode: true,
 });
 
 export default apolloClient;
