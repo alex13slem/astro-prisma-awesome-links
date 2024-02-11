b23004f1501bb2975856bbe9bbaae7f07978ef65
alex13slem <a.s.scherba@yandex.by>
2024-02-11 11:30:54 +0300

    asd

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,9 +1,27 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client/core';
 
-const apolloClient = new ApolloClient({
-  uri: (import.meta.env.URL || '') + '/api/graphql',
-  cache: new InMemoryCache(),
-  ssrMode: true,
-});
+// const apolloClient = new ApolloClient({
+//   uri: (import.meta.env.URL || '') + '/api/graphql',
+//   cache: new InMemoryCache(),
+//   ssrMode: true,
+// });
+
+// export default apolloClient;
+
+const apolloClientSingleton = () =>
+  new ApolloClient({
+    uri: (import.meta.env.URL || '') + '/api/graphql',
+    cache: new InMemoryCache(),
+    ssrMode: true,
+  });
+
+declare global {
+  var apolloClient: undefined | ReturnType<typeof apolloClientSingleton>;
+}
+
+const apolloClient = globalThis.apolloClient || apolloClientSingleton();
+
+if (process.env.NODE_ENV !== 'production')
+  globalThis.apolloClient = apolloClient;
 
 export default apolloClient;
