d85d1d5917f9ebc66ea62809df5f0f704e6f8ce0
alex13slem <a.s.scherba@yandex.by>
2024-02-11 13:40:34 +0300

    asd

---
+++ src/lib/urqlClient.ts
@@ -0,0 +1,20 @@
+import { Client, cacheExchange, fetchExchange } from '@urql/core';
+
+const url =
+  (import.meta.env.DEV ? 'http://localhost:8888' : import.meta.env.SITE) +
+  '/api/graphql';
+
+declare global {
+  var urqlClient: undefined | Client;
+}
+
+const urqlClient =
+  globalThis.urqlClient ||
+  new Client({
+    url,
+    exchanges: [cacheExchange, fetchExchange],
+  });
+
+if (import.meta.env.DEV) globalThis.urqlClient = urqlClient;
+
+export default urqlClient;
